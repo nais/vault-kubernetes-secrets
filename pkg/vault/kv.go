bb3eedd103269dccd6ec1dd049d1fc9cd373783e
Eirik Sletteberg <eirik.sletteberg@nav.no>
2019-07-03 15:36:13 +0200

    Support k/v stores version 2.0 (versioned but with another JSON data format from Vault)

--- pkg/vault/kv.go
+++ pkg/vault/kv.go
@@ -2,6 +2,7 @@ package vault
 
 import (
 	"fmt"
+	"reflect"
 )
 
 type KV interface {
@@ -17,8 +18,30 @@ func (c *client) Get(path, token string) (map[string]string, error) {
 
 	secrets := make(map[string]string)
 
+	// If the map only contains two keys; "data" and "metadata", we assume it's a
+	// kv store version 2.0. So return everything in the "data" key.
 	if secret == nil {
 		return secrets, nil
+	}
+
+	// Heuristic; check if we only get two keys ("data" and "metadata") and guess that it's kv version 2.
+	if metadata, ok := secret.Data["metadata"]; ok {
+		if data, ok := secret.Data["data"]; ok {
+			if len(secret.Data) == 2 {
+				if reflect.TypeOf(metadata).Kind() == reflect.Map && reflect.TypeOf(data).Kind() == reflect.Map {
+					hello := data.(map[string]interface{})
+					for k, v := range hello {
+						switch val := v.(type) {
+						case string:
+							secrets[k] = val
+						default:
+							return nil, fmt.Errorf("vault: %s[%s] has type %T", path, k, val)
+						}
+					}
+					return secrets, nil
+				}
+			}
+		}
 	}
 
 	for k, v := range secret.Data {
--- pkg/vault/kv_test.go
+++ pkg/vault/kv_test.go
@@ -18,6 +18,25 @@ var sampleKvResponse = `{
   "renewable": false
 }`
 
+var sampleKvResponseV2 = `{
+  "auth": null,
+  "data": {
+    "data": {
+    	"foo": "bar",
+		"bar": "foo"
+    },
+    "metadata": {
+		"created_time": "2019-07-03T10:38:09.060969744Z",
+		"deletion_time": "",
+		"destroyed": false,
+		"version": 1
+    }
+  },
+  "lease_duration": 2764800,
+  "lease_id": "",
+  "renewable": false
+}`
+
 func TestKV(t *testing.T) {
 	defer gock.Off()
 
@@ -29,6 +48,30 @@ func TestKV(t *testing.T) {
 			MatchHeader("X-Vault-Token", token).
 			Reply(200).
 			JSON(sampleKvResponse)
+
+		c := NewKVClient(ClientOptions{
+			Server: "http://vault.foo.bar",
+			HTTPClient: &http.Client{
+				Transport: http.DefaultTransport,
+			},
+		})
+
+		secrets, e := c.Get("secret", token)
+		st.Assert(t, e, nil)
+		st.Assert(t, len(secrets), 2)
+		st.Assert(t, secrets["foo"], "bar")
+		st.Assert(t, secrets["bar"], "foo")
+		st.Assert(t, gock.IsDone(), true)
+	})
+
+	t.Run("Calling KV backend with a token should return secrets - KV version 2", func(t *testing.T) {
+		token := "token"
+
+		gock.New("http://vault.foo.bar").
+			Get("/v1/secret").
+			MatchHeader("X-Vault-Token", token).
+			Reply(200).
+			JSON(sampleKvResponseV2)
 
 		c := NewKVClient(ClientOptions{
 			Server: "http://vault.foo.bar",
